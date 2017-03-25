82cead32000435b18787862f923005ae905c5797
Heriberto David <heriberto@MacBook-Air-de-Heriberto.local>
2017-03-25 16:52:01 -0500

    Initial Commit

---
+++ CrasH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrasH
+//
+//  Created by Heriberto David Yepes on 25/03/17.
+//  Copyright © 2017 Heriberto David Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
